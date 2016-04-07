2f1c9b9701a3ca32f5a56d938818a9f49aea549c
Prakhar Garg <prakhargarg@utexas.edu>
2016-04-07 18:21:21 -0500

    shitty static page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407203333) do
+ActiveRecord::Schema.define(version: 20160407230313) do
+
+  create_table "charities", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
