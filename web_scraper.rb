3aed7e492dd11c7c38f15152574a323d0800589c
Prakhar Garg <prakhargarg@utexas.edu>
2016-04-09 17:28:21 -0500

    eh

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,5 +1,13 @@
-require 'HTParty'
+require 'HTTParty'
 require 'Nokogiri'
 require 'JSON'
 require 'Pry'
 require 'csv'
+
+page = HTTParty.get('http://austin.craigslist.org/search/pet?s=0')
+
+parse_page = Nokogiri::HTML(page)
+
+bikes_array = []
+
+Pry.start(binding)
