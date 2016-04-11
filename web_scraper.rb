82e028ad12921633fdbe4c1ee3dbe12632d3a7f4
Prakhar Garg <prakhargarg@utexas.edu>
2016-04-11 17:13:53 -0500

    trying to scrape form 990s

--- web_scraper.rb
+++ web_scraper.rb
@@ -1,11 +1,32 @@
 require 'rubygems'
 require 'mechanize'
 
-# Instatiate a new mechanize object
+# # Instatiate a new mechanize object
+# agent = Mechanize.new
+# # # Use the agent to fetch a page
+# page = agent.get('http://google.com/')
+# # List all of the links on the homepage
+# # page.links.each do |link|
+# #   puts link.text
+# # end
+#
+# # Fetch the form off the page
+# google_form = page.form('f')
+# # Access the input field 'q' and put in 'ruby mechanize'
+# # google_form.q = 'ruby mechanize'
+# # # Submit the form
+# # page = agent.submit(google_form,google_form.buttons.first)
+#
+# pp page
+
+##########Scraping Data##########
+
 agent = Mechanize.new
-# Use the agent to fetch a page
-page = agent.get('http://google.com/')
-# List all of the links on the homepage
-# page.links.each do |link|
-#   puts link.text
-# end
+base_page  = agent.get('http://foundationcenter.org/findfunders/990finder/')
+
+base_page.form.radiobuttons_with(:name => '990_type')[1].check
+pp base_page
+form_990_form = base_page.form('nil')
+form_990_form.fn = "test"
+
+pp base_page
