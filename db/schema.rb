e8c4c3fda5fda6447f0c65c1c13d7c9c2e85ee83
Prakhar Garg <prakhargarg@utexas.edu>
2016-04-11 19:30:59 -0500

    fixed models

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407230313) do
+ActiveRecord::Schema.define(version: 20160412002713) do
 
   create_table "charities", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
