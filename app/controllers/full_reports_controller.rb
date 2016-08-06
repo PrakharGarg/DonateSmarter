24b1f11db85ef12897cbfb294da4dcb59bd119fd
PrakharGarg <prakhargarg@utexas.edu>
2016-08-06 08:03:25 -0400

    cleaning up code

--- app/controllers/full_reports_controller.rb
+++ app/controllers/full_reports_controller.rb
@@ -4,73 +4,9 @@ class FullReportsController < ApplicationController
   def index
   end
   
-  # Let users download a csv for a select charity. 
-  # Param: 
-  # => Charity Name
-  def csv
-    filename = "All_Charities.csv"
   
-    # Streaming the result
-    self.response.headers["Content-Type"] ||= 'text/csv'
-    self.response.headers["Content-Disposition"] = "attachment; filename=#{filename}"
-    self.response.headers["Content-Transfer-Encoding"] = "binary"
-    self.response.headers["Last-Modified"] = Time.now.ctime.to_s
-    
-    a = Hash.from_xml(File.read("./cfc.xml"))
-    field_names = []
-    field_values = []
-    recrusive_find_hash(a,field_names,field_values)
-    
-    self.response_body = Enumerator.new do |yielder|
-      
-      yielder << field_names.to_csv
-      yielder << field_values.to_csv
-      
-    end
-end
+  def csv
+  end
 
-def recrusive_find_hash(hash,field_names,values)
-    hash.each do |key,value|
-      if value.is_a? Hash 
-        recrusive_find_hash(value,field_names,values)
-      else
-        field_names.push(key.to_s)
-        values.push(value.to_s)
-      end
-    end
 end
-    # self.response_body = Enumerator.new do |yielder|
-    #   
-    #   yielder << index_header_names().to_csv
-    #   
-    #   
-      
-      # fees.find_each(batch_size: 100) do |donation|
-      #   if (donation.fee_name.include? "Champions")
-      #     exhibitor_id = donation.exhibitor_id
-      #     fee_name = donation.fee_name
-      #     fee_quantity = donation.quantity
-      #     fee_amount = "$" + (donation.fee_amount * fee_quantity).to_s 
-      #     fee_created_at = donation.created_at
-      #     exhibitor = Exhibitor.find(exhibitor_id)
-      #     customer = exhibitor.customer
-      #     data_row = [
-      #       exhibitor.first_name,
-      #       exhibitor.last_name,
-      #       exhibitor.address,
-      #       exhibitor.city,
-      #       exhibitor.zip_code,
-      #       customer.email,
-      #       exhibitor.customer_id,
-      #       fee_name,
-      #       fee_quantity,
-      #       fee_amount,
-      #       fee_created_at
-      #     ]
-          
-          # yielder << data_row.to_csv
-          
-        # end
-    #   end
-    # end
-end
+  
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,18 +1,26 @@
 class WelcomeController < ApplicationController
+  # Use the devise gem to authenticate that a user is signed in before the page
+  # renders. 
   before_action :authenticate_user!
+  # Use the autocomplete gem to autocomplete the charity search
   autocomplete :charity, :name
+
   def index
   end
   
-  def add_charities 
-    parse_list = JSON.parse(open("https://s3.amazonaws.com/irs-form-990/index.json").read)
-    # parse_list = JSON.parse(json)
-    charity_list = parse_list["AllFilings"]
-    charity_list.each do |charity|
-      if (charity["IsAvailable"] == true && (Charity.exists?(name: charity["OrganizationName"]) == false))
-          Charity.create(name: charity["OrganizationName"], url: charity["URL"])
-      end
-    end
-  end 
+  # THIS SHOULD NOT BE RUN. This is a nuke. Takes about 3 days to run and 
+  # parse all of the charities into the database. #TODO add a tax year date
+  # to the database so I know I have the most recent data. 
+  # def add_charities 
+  #   json = File.read("./charity_list.json")
+  #   parse_list = JSON.parse(json)
+  #   # parse_list = JSON.parse(json)
+  #   charity_list = parse_list["AllFilings"]
+  #   charity_list.each do |charity|
+  #     if (charity["IsAvailable"] == true && (Charity.exists?(name: charity["OrganizationName"]) == false))
+  #         Charity.create(name: charity["OrganizationName"], url: charity["URL"])
+  #     end
+  #   end
+  # end 
   
 end
